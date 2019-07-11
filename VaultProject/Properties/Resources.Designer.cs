a00eb731333d19e89e52fd22753efe66bba540c0
Max Shymchuk <octorix@mail.ru>
2019-07-11 21:08:57 +0300

    Project reference update

--- VaultProject/Properties/Resources.Designer.cs
+++ VaultProject/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Vault.Properties {
+namespace VaultProject.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Vault.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Vault.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VaultProject.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- VaultProject/Properties/Settings.Designer.cs
+++ VaultProject/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Vault.Properties {
+namespace VaultProject.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
